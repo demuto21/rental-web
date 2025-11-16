85365bd4c70b34591a5ebd4d5ec46175c6f3e1fd
FEZE Djoumessi <fezefred29@gmail.com>
2025-11-16 14:59:50 +0100

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... vous avez peut-être d'autres configurations ici ...
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**', // '/**' signifie "autoriser tous les chemins sur ce site"
+      },
+      // Vous pouvez ajouter d'autres domaines ici si besoin
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
