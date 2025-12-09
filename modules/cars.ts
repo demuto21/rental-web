4f9962159a984441c65394ff5a3c96ab8792e893
FEZE Djoumessi <fezefred29@gmail.com>
2025-12-09 12:45:52 +0100

    Add files via upload

---
+++ modules/cars.ts
@@ -0,0 +1,40 @@
+// data/cars.ts
+export const cars = [
+  {
+    id: 1,
+    name: "Toyota Camry",
+    price: 100000,
+    currency: "CFA/jr",
+    image: "/car-red.png", // Remplace par tes images locales
+    rating: 2,
+    seats: 2,
+    transmission: 2,
+    tag: "detail",
+    isFavorite: true,
+  },
+  {
+    id: 2,
+    name: "Toyota Camry",
+    price: 100000,
+    currency: "CFA/jr",
+    image: "/car-red.png",
+    rating: 2,
+    seats: 2,
+    transmission: 2,
+    tag: "detail",
+    isFavorite: true,
+  },
+  {
+    id: 3,
+    name: "Toyota Camry",
+    price: 100000,
+    currency: "CFA/jr",
+    image: "/car-red.png",
+    rating: 2,
+    seats: 2,
+    transmission: 2,
+    tag: "detail",
+    isFavorite: true,
+  },
+  // Ajoute d'autres voitures pour remplir la grille
+];
